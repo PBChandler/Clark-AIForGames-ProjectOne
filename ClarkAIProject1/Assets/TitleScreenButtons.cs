3b279463a195d8924ffc3ff447be4ca2a6589d5e
PBChandler <prestonbchandler@gmail.com>
2025-09-08 22:51:41 -0400

    title screen done

---
+++ ClarkAIProject1/Assets/TitleScreenButtons.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TitleScreenButtons : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);
+    }
+}
